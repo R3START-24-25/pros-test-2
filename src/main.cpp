2eb1f3a9a388afd6d441969f354bfe91a1265cad
Shayan Nezami <me@shayannezami.co.uk>
2024-08-17 16:02:49 +0100

    linear-ish 1 done w/ neg values)

--- src/main.cpp
+++ src/main.cpp
@@ -21,11 +21,21 @@ void opcontrol() {
 		int dir = master.get_analog(ANALOG_LEFT_Y);
 		int turn = master.get_analog(ANALOG_RIGHT_X);
 
-        if (dir <= 80) dir *= exponential_a;
-        else dir = 127;
+        //if (dir <= 80) dir *= exponential_a;
+        //else dir = 127;
 
-        if (turn <= 80) turn *= exponential_a;
-        else turn = 127;
+        //if (turn <= 80) turn *= exponential_a;
+        //else turn = 127;
+        
+        if (dir > 2 && dir < 35) dir = 20;
+        else if (dir > 90) dir = 127;
+        else if (dir < -2 && dir > -35) dir = -20;
+        else if (dir < -90) dir = -127;
+        
+        if (turn > 2 && turn < 35) turn = 20;
+        else if (turn > 90) turn = 127;
+        else if (turn < -2 && turn > -35) turn = -20;
+        else if (turn < -90) turn = -127;
 
 		left_mg.move(- turn - dir);
 		right_mg.move(- turn + dir);
