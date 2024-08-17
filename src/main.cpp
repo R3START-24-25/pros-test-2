aaf02a44004e3bac47ebce8c622da98cd5f13d1a
Shayan Nezami <me@shayannezami.co.uk>
2024-08-17 15:50:38 +0100

    log 1 tested

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <cmath>
 
 void initialize() {}
 
@@ -11,12 +12,20 @@ void autonomous() {}
 
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({8, 9, 10});
+	pros::MotorGroup left_mg({8, 7, 10});
 	pros::MotorGroup right_mg({1, 2, 3});
+
+    const float exponential_a = pow(10, (log10(127)/80));
 
 	while (true) {
 		int dir = master.get_analog(ANALOG_LEFT_Y);
 		int turn = master.get_analog(ANALOG_RIGHT_X);
+
+        if (dir <= 80) dir *= exponential_a;
+        else dir = 127;
+
+        if (turn <= 80) turn *= exponential_a;
+        else turn = 127;
 
 		left_mg.move(- turn - dir);
 		right_mg.move(- turn + dir);
